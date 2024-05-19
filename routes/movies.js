a8729c8c7d96c149ffec21104af3b21b517c1de2
Agoston Diamont <dagoston93@gmail.com>
2024-05-19 23:52:59 +0200

    route handlers implemented.

--- routes/movies.js
+++ routes/movies.js
@@ -3,4 +3,76 @@ const { Movie, validate } = require("../models/movie");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("name");
+  res.send(movies);
+});
+
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+
+  if (!movie) {
+    res.status(404).send("The movie with the given ID was not found.");
+    return;
+  }
+  res.send(movie);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  let movie = new Movie({
+    title: req.body.title,
+    genre: req.body.genre,
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+
+  movie = await movie.save();
+  res.send(movie);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      genre: req.body.genre,
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    },
+    { new: true }
+  );
+
+  if (!movie) {
+    res.status(404).send("The movie with the given ID was not found.");
+    return;
+  }
+
+  res.send(movie);
+});
+
+router.delete("/:id", async (req, res) => {
+  const movie = await Movie.findByIdAndDelete(req.params.id);
+
+  if (!movie) {
+    res.status(404).send("The movie with the given ID was not found.");
+    return;
+  }
+
+  res.send(movie);
+});
+
 module.exports = router;
