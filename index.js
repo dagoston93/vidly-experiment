125c0fe70735c4c6ca549f578a6f107cf7ed79a7
Agoston Diamont <dagoston93@gmail.com>
2024-05-20 00:23:29 +0200

    rentals API added.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 app.get("/", (req, res) => {
   res.send("Vidly!");
