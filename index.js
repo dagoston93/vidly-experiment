140e8b3fd1aad6f4744fd3f21b1186cdec32c1de
Agoston Diamont <dagoston93@gmail.com>
2024-05-19 23:41:48 +0200

    Movies router added.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 
 app.get("/", (req, res) => {
   res.send("Vidly!");
