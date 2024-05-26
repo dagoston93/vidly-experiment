8d03f8d372553968ade4bfd1040e4060897ef222
Agoston Diamont <dagoston93@gmail.com>
2024-05-26 19:47:18 +0200

    Test cases and implementation added to handle missing request params.

--- routes/returns.js
+++ routes/returns.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Unauthorized");
+  if (!req.body.customerId) return res.status(400).send("customerId needed");
+  if (!req.body.moviedId) return res.status(400).send("movieId needed");
 });
 
 module.exports = router;
