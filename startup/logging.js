df50a95211a1cf62ecce96c29e519fc11a1684bd
Agoston Diamont <dagoston93@gmail.com>
2024-05-28 20:49:44 +0200

    logging methods re-enabled.

--- startup/logging.js
+++ startup/logging.js
@@ -6,20 +6,20 @@ require("express-async-errors");
 const db = config.get("db");
 
 module.exports = function () {
-  // winston.add(
-  //   new winston.transports.File({
-  //     filename: "logfile.log",
-  //     handleExceptions: true,
-  //     handleRejections: true,
-  //   })
-  // );
+  winston.add(
+    new winston.transports.File({
+      filename: "logfile.log",
+      handleExceptions: true,
+      handleRejections: true,
+    })
+  );
 
-  // winston.add(
-  //   new winston.transports.MongoDB({
-  //     db: db,
-  //     level: "error",
-  //   })
-  // );
+  winston.add(
+    new winston.transports.MongoDB({
+      db: db,
+      level: "error",
+    })
+  );
 
   //if (!process.env.NODE_ENV || process.env.NODE_ENV !== "production") {
   winston.add(
